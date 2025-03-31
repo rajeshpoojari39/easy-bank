92c1f7634287cc3a65b8e9943f4dd827d15c57b1
Rajesh Poojari <rajeshpoojari39@gmail.com>
2025-03-31 11:24:25 +0530

    challenge done

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector("#btmHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open"); // Open Hamburger Menu
+    fadeElems.forEach((element) => {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
+});
